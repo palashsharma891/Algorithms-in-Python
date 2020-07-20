33b763e141aea8364f56ebfe12a6790ef79fa9e5
Palash Sharma <54630543+palashsharma891@users.noreply.github.com>
2020-07-20 15:32:35 +0530

    Create generalTree.py

---
+++ 2. Trees/1. General Trees/generalTree.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 20 11:51:52 2020
+
+@author: palash
+"""
+
+
+class Node:
+    def __init__(self, item):
+        self.left = None
+        self.right = None
+        self.val = item
+
+
+def inorder(root):
+
+    if root:
+        # Traverse left
+        inorder(root.left)
+        # Traverse root
+        print(str(root.val) + "->", end='')
+        # Traverse right
+        inorder(root.right)
+
+
+def postorder(root):
+
+    if root:
+        # Traverse left
+        postorder(root.left)
+        # Traverse right
+        postorder(root.right)
+        # Traverse root
+        print(str(root.val) + "->", end='')
+
+
+def preorder(root):
+
+    if root:
+        # Traverse root
+        print(str(root.val) + "->", end='')
+        # Traverse left
+        preorder(root.left)
+        # Traverse right
+        preorder(root.right)
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Inorder traversal ")
+inorder(root)
+
+print("\nPreorder traversal ")
+preorder(root)
+
+print("\nPostorder traversal ")
+postorder(root)
